d4bf8517020a3b8982fefdacd37f0318c57fa760
KevinLoayzaS <kevin28042001@gmail.com>
2023-09-29 10:17:17 +0200

    29 SEPTIEMBRE

--- src/Ejercicios_ManejoFicheros/Ejercicio6.java
+++ src/Ejercicios_ManejoFicheros/Ejercicio6.java
@@ -1,9 +1,4 @@
 package Ejercicios_ManejoFicheros;
 
 public class Ejercicio6 {
-    public static void main(String[] args) {
-
-
-
-    }
 }
--- src/Ejercicios_ManejoFicheros/Ejercicio8.java
+++ src/Ejercicios_ManejoFicheros/Ejercicio8.java
@@ -5,9 +5,8 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 
-public class Ejercicio7 {
+public class Ejercicio8 {
     public static void main(String[] args) {
-
         String ruta_fichero= "C:\\Users\\KevinLoayzaS\\IdeaProjects\\Acceso_Datos\\NUEVODIR\\FICHERO1.txt" ;
         File fichero1 = new File(ruta_fichero);
 
@@ -15,24 +14,19 @@ public class Ejercicio7 {
         File fichero2 = new File(ruta_fichero2);
 
         //TAMBIEN PODEMOS COPIARLO EN UNA CARPETA SUPERIOR, EN ESTE CASO SE LLAMARIA "Acceso_Datos", seria poner la ruta hasta ahi y ya funcionaria
-        String ruta_directorio= "C:\\Users\\KevinLoayzaS\\IdeaProjects\\Acceso_Datos\\NUEVODIR" ;
+        String ruta_directorio= "C:\\Users\\KevinLoayzaS\\IdeaProjects\\Acceso_Datos" ;
         File destino = new File(ruta_directorio);
 
-
-
         try {
-            FileUtils.copyFileToDirectory(fichero1, destino);
-            System.out.println("Fichero "+fichero1.getName()+ " copiado");
+            FileUtils.moveFileToDirectory(fichero1,destino,true);
+            System.out.println("El fichero "+fichero1.getName()+ " ha sido movido a "+destino.getName());
 
-            FileUtils.copyFileToDirectory(fichero2, destino);
-            System.out.println("Fichero "+fichero2.getName() + " copiado");
-        }
+            FileUtils.moveFileToDirectory(fichero2,destino,true);
+            System.out.println("El fichero "+fichero2.getName()+ " ha sido movido a "+destino.getName());
 
-        catch (IOException e){
-            System.err.println("Error al copiar el archivo");
+        }catch (IOException e){
+            System.err.println("Error al mover el archivo");
             e.printStackTrace();
         }
-
-
     }
 }
